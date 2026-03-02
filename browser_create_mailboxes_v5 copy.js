e0567743deb4be3466f0f396eb94071c485d34cb
ivimudry <ivimudry@users.noreply.github.com>
2026-03-02 12:34:06 +0100

    auto: 2026-03-02 12:34 (1 files)

--- browser_create_mailboxes_v5 copy.js
+++ browser_create_mailboxes_v5 copy.js
@@ -10,7 +10,7 @@
                        // номер телефону з тригера, з видаленням "+" якщо є, щоб формат відповідав вимогам CM.com
                    }
                ],
-               "minimumNumberOfMessageParts": 1,   // мінімум 1 частина SMS (153)
+               "minimumNumberOfMessageParts": 1,   // мінімум 1 частина SMS (153 символів для GSM (латиниця, цифри) або 70 для Unicode)
                "maximumNumberOfMessageParts": 3,   // максимум 3 частини (якщо текст довгий)
                "body": {
                    "type": "auto",            // автоматичне визначення кодування (GSM або Unicode)
