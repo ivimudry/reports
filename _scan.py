42262eb3d3892368295953be780fc95c97e29dc7
ivimudry <ivimudry@users.noreply.github.com>
2026-04-23 13:31:05 +0200

    auto: 2026-04-23 13:31 (1 files)

---
+++ _scan.py
@@ -0,0 +1,7 @@
+﻿import re, sys
+p=r'C:\Projects\REPORTS\тексти\оші\html\Oshi Welcome Flow Updated.html'
+s=open(p,'r',encoding='utf-8').read()
+lines=s.split('\n')
+for i,l in enumerate(lines,1):
+    if re.search(r'EMAIL #|INAPP #|SMS #|DEFAULT|\(AU\)|\(CA\)|\(DE\)', l):
+        print(i, l[:200])
